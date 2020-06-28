9cea2a17afa3b6b5992a16ff2660efdede17aae0
LuisCampi <lcampi@espol.edu.ec>
2020-06-28 16:41:53 -0500

    PROGRAMA FINAL CORREGIDO

--- src/Principal/Sistema.java
+++ src/Principal/Sistema.java
@@ -24,31 +24,32 @@ public class Sistema {
         //Producir Helado
     	Postre helado_vainilla = new Helado("Vainilla");
     	arrPostres.add(helado_vainilla);
-    	System.out.println(helado_vainilla);
     	
     	//Producir Pastel
     	Postre pastel_chocolate = new Pastel("Chocolate");
     	arrPostres.add(pastel_chocolate);
-    	System.out.println(pastel_chocolate);
     	
-    	//Añadir aderezos tanto al helado como al pastel
+    	//Crear instancias de aderezos crema y frutilla
     	Aderezo aderezo_crema = new Crema();
     	aderezo_crema.setNombre();
     	Aderezo aderezo_frutilla = new Frutilla();
     	aderezo_frutilla.setNombre();
-		Postre.anadirAderezoPostre(helado_vainilla, aderezo_crema);
-		Postre.anadirAderezoPostre(helado_vainilla, aderezo_frutilla);
+    	
+    	//Añadir aderezos al helado
+		OperacionesPostre.anadirAderezoPostre(helado_vainilla, aderezo_crema);
+		OperacionesPostre.anadirAderezoPostre(helado_vainilla, aderezo_frutilla);
 		System.out.println(helado_vainilla);
-		Postre.anadirAderezoPostre(pastel_chocolate, aderezo_frutilla);
-		Postre.anadirAderezoPostre(pastel_chocolate, aderezo_crema);
+		
+		//Mostrar el precio final del helado
+		System.out.println(mnj_precio.showPrecioFinal(helado_vainilla));
+		
+		//Añadir aderezos al pastel
+		OperacionesPostre.anadirAderezoPostre(pastel_chocolate, aderezo_frutilla);
+		OperacionesPostre.anadirAderezoPostre(pastel_chocolate, aderezo_crema);
 		System.out.println(pastel_chocolate);
 		
-		//Mostrar el precio final de cada postre
-		System.out.println(mnj_precio.showPrecioFinal(helado_vainilla));
+		//Mostrar el precio final del pastel
 		System.out.println(mnj_precio.showPrecioFinal(pastel_chocolate));
-		
-		//Mostar el arreglo final
-		System.out.println(arrPostres);
 		
         mnj_leche.cambiarTipoLeche(new LecheDeslactosada(), helado_vainilla);
 		//mnj_leche.cambiarTipoLeche(new LecheDescremada(), helado_vainilla);
